d6d721cd0889921f01f6144a97dc730459dfa6af
Afonso Inglês <afonsorodriguesingles@gmail.com>
2025-05-12 22:29:15 +0100

    add / route

--- api/main.py
+++ api/main.py
@@ -6,3 +6,7 @@ app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None, title="Helpers AP
 
 app.include_router(api.routers.notifications.router, prefix="/v1/notifications", tags=["Notifications"])
 app.include_router(api.routers.webhooks.router, prefix="/v1/webhooks", tags=["Webhooks"])
+
+@app.get("/")
+async def root():
+    return {"message": "hello world"}
