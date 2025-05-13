c2901c1c2b57a0e5b4f05aef20390b906260e5ac
Afonso Inglês <afonsorodriguesingles@gmail.com>
2025-05-13 10:42:27 +0000

    Use GitHub Actions instead of GitHub webhooks

--- api/main.py
+++ api/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 import api.routers.notifications
-import api.routers.webhooks
 
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None, title="Helpers API", version="1.0.0")
 
 app.include_router(api.routers.notifications.router, prefix="/v1/notifications", tags=["Notifications"])
-app.include_router(api.routers.webhooks.router, prefix="/v1/webhooks", tags=["Webhooks"])
 
 @app.get("/")
 async def root():
