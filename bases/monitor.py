bcbf8aae25b568b712cb306a19ddc0a92e3a3d12
Afonso Inglês <afonsorodriguesingles@gmail.com>
2025-05-20 23:20:34 +0100

    removing monitors

--- bases/monitor.py
+++
@@ -1,9 +0,0 @@
-class Monitor:
-    def __init__(self, enabled=True):
-        self.enabled = enabled
-
-    def run(self):
-        raise NotImplementedError("Each monitor must implement the 'run' method.")
-    
-    def stop(self):
-        raise NotImplementedError("Each monitor must implement the 'stop' method.")
