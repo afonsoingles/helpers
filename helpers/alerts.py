cf8363ab74769c7bc68c3e22a3fa00a2bc26fe6b
Afonso Inglês <afonsorodriguesingles@gmail.com>
2025-05-26 22:11:02 +0100

    Start Alerts - save occurrences in db.

---
+++ helpers/alerts.py
@@ -0,0 +1,42 @@
+from bases.helper import BaseHelper
+from datetime import datetime
+import os
+import requests
+import schedule
+from utils.mongoHandler import MongoHandler
+
+mongo = MongoHandler()
+
+class alerts(BaseHelper):
+    def __init__(self):
+        super().__init__(run_at_start=True)
+
+    def run(self):
+        print("[alerts] Started at: ", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+        fetchAlerts = requests.get("https://api-dev.fogos.pt/v2/incidents/active").json()
+        for incident in fetchAlerts.get("data", []):
+            mongo.db.occurrences.update_one(
+                {"id": incident["id"]},
+                {"$set": {
+                    "id": incident["id"],
+                    "date": incident["date"],
+                    "hour": incident["hour"],
+                    "air": incident["aerial"],
+                    "water": incident["meios_aquaticos"],
+                    "man": incident["man"],
+                    "dico": incident["dico"],
+                    "lat": incident["lat"],
+                    "lon": incident["lng"],
+                    "typeId": incident["naturezaCode"],
+                    "type": incident["natureza"],
+                    "status": incident["status"],
+                    "statusCode": incident["statusCode"],
+                }},
+                upsert=True
+            )
+        print(f"[alerts] Updated {len(fetchAlerts.get('data', []))} occurrences in the database")
+        print("[alerts] Finished at: ", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+    def schedule(self):
+        schedule.every(5).minutes.do(self.run)
