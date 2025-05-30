17ef240fe210fffdd7d771c837d32e1967b75798
Afonso Inglês <afonsorodriguesingles@gmail.com>
2025-05-30 22:22:57 +0100

    Sound parameter is now supported

--- utils/pusher.py
+++ utils/pusher.py
@@ -3,7 +3,7 @@ import os
 
 class Pusher:
 
-    def singlePush(self, to, title, body, data, ttl, isCritical=False):
+    def singlePush(self, to, title, body, data, ttl, sound, isCritical=False):
         try:
             r = requests.post(f"{os.environ.get('API:URL')}/v1/notifications/send", headers={
                 "X-Secure-Key": os.environ.get("SECURE_KEY"),
@@ -14,12 +14,13 @@ class Pusher:
                 "body": body,
                 "data": data,
                 "ttl": ttl,
+                "sound": sound,
                 "isCritical": isCritical,
             })
         except:
             return
     
-    def bulkPush(self, title, body, data, ttl, isCritical=False):
+    def bulkPush(self, title, body, data, ttl, sound, isCritical=False):
         try:
             r = requests.post(f"{os.environ.get('API_URL')}/v1/notifications/send", headers={
                 "X-Secure-Key": os.environ.get("SECURE_KEY"),
@@ -30,6 +31,7 @@ class Pusher:
                 "body": body,
                 "data": data,
                 "ttl": ttl,
+                "sound": sound,
                 "isCritical": isCritical,
             })
         except:
