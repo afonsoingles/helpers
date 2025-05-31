449bdfb270771558432ee77681eee4fda0ba307c
Afonso Inglês <afonsorodriguesingles@gmail.com>
2025-05-31 21:42:36 +0100

    Add return type hint to get_latest_commit method in GitHub class

--- utils/github.py
+++ utils/github.py
@@ -1,5 +1,5 @@
 import subprocess
 
 class GitHub:
-    def get_latest_commit():
+    def get_latest_commit(args=None, **kwargs) -> str:
         return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
