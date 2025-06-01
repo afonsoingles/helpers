62d9a70d919401ca2039bee740c3474f71164d0a
Afonso Inglês <afonsorodriguesingles@gmail.com>
2025-06-01 23:38:47 +0100

    Fix

--- helpers/bus.py
+++ helpers/bus.py
@@ -53,10 +53,9 @@ class busAlerts(BaseHelper):
                 continue
             
 
-            
+            currentTime = datetime.now()
 
-            busEstimatedArrival = datetime.fromtimestamp(nextBus["estimated_arrival_unix"]).astimezone(datetime.now().astimezone().tzinfo)
-            currentTime = datetime.now().astimezone()
+            busEstimatedArrival = datetime.fromtimestamp(nextBus["estimated_arrival_unix"])
             userReminder = user["userConfig"]["bus"]["reminderTime"]
             timeDiff = (busEstimatedArrival - currentTime).total_seconds()
             busScheduledArrival = datetime.fromtimestamp(nextBus["scheduled_arrival_unix"])
