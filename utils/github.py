2ad8219ff4f51c5c74d1236b1ee0bdd31cec01eb
Afonso Inglês <afonsorodriguesingles@gmail.com>
2025-06-03 16:28:49 +0100

    Use CWD in the get_latest_commit

--- utils/github.py
+++ utils/github.py
@@ -1,5 +1,6 @@
 import subprocess
+import os
 
 class GitHub:
     def get_latest_commit(args=None, **kwargs) -> str:
-        return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
+        return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], cwd=os.getcwd()).decode('ascii').strip()
