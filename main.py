387846704aa997e6e96406dbd6456b35e3b9b45f
Afonso Inglês <afonsorodriguesingles@gmail.com>
2025-06-03 22:12:18 +0100

    Refactor Sentry integration and error handling in main process

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
 import asyncio
 import os
 import schedule
-import logging
 from dotenv import load_dotenv
 from utils.startup import Startup
 from utils.logger import Logger
+from utils.github import GitHub
 import sentry_sdk
 from sentry_sdk.integrations.logging import LoggingIntegration
-import traceback
 
 load_dotenv()
 
 startup = Startup()
 logger = Logger()
+gh = GitHub()
 
 sentry_sdk.init(
     dsn=os.environ.get("SENTRY_DSN"),
     send_default_pii=True,
     _experiments={"enable_logs": True},
     integrations=[
-        LoggingIntegration(
-            level=logging.INFO,
-            event_level=logging.ERROR
-        ),
+        LoggingIntegration(),
     ],
     traces_sample_rate=1.0,
-    environment=os.environ.get("DB_ENV")
+    environment=os.environ.get("DB_ENV"),
+    enable_tracing=True,
+    release=gh.get_latest_commit(),
 )
 
 global apiProcess
@@ -40,7 +39,6 @@ async def main():
     logger.info("[STARTUP] Starting up...")
     helpers = startup.discover_helpers()
     logger.info(f"[STARTUP] Found {len(helpers)} helpers.")
-
     try:
         apiProcess = await asyncio.create_subprocess_exec(
             "python", "-m", "uvicorn", "api.main:app",
@@ -49,24 +47,22 @@ async def main():
             "--log-level", os.environ.get("API_LOG_LEVEL", "info")
         )
 
-
         runningHelpers = [
             asyncio.create_task(startup.run_helper(helper)) for helper in helpers
         ]
-
         logger.info("[STARTUP] Startup complete. Waiting for scheduled tasks...")
         while True:
             try:
                 schedule.run_pending()
                 await asyncio.sleep(1)
-            except Exception:
-                print(f"{traceback.format_exc()}")
+            except Exception as e:
+                logger.error("Error in scheduled tasks", e)
 
-    except KeyboardInterrupt:
+    except KeyboardInterrupt as e:
         logger.info("[SHUTDOWN] Keyboard interrupt received.")
         startup.force_exit(logger, apiProcess)
-    except Exception:
-        logger.error(traceback.format_exc())
+    except Exception as e:
+        logger.error("Error running startup process", e)
         startup.force_exit(logger, apiProcess)
     finally:
         startup.force_exit(logger, apiProcess)
@@ -74,9 +70,8 @@ async def main():
 if __name__ == "__main__":
     try:
         asyncio.run(main())
-    except KeyboardInterrupt:
-        logger.info("[SHUTDOWN] Keyboard interrupt received.")
+    except KeyboardInterrupt as e:
+        logger.error("[SHUTDOWN] Keyboard interrupt received.", e)
         startup.force_exit(logger, apiProcess)
-    except Exception:
-        logger.error(traceback.format_exc())
-        traceback.print_exc()
+    except Exception as e:
+        logger.error("Error in main thread", e)
