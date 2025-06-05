6c6abd42648291b474bcdd4f1876adce71dcbf02
Afonso Inglês <afonsorodriguesingles@gmail.com>
2025-06-05 23:22:50 +0100

    Replace print statements with logger in rulesUpdater for improved logging consistency

--- helpers/rulesUpdater.py
+++ helpers/rulesUpdater.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import os
 import json
 from utils.airtabler import Airtabler
+from main import logger
 
 airtabler = Airtabler(base_id=os.environ.get("AIRTABLE_BASE_ID"), api_key=os.environ.get("AIRTABLE_API_KEY"))
 
@@ -12,25 +13,22 @@ class rulesUpdater(BaseHelper):
         super().__init__(run_at_start=True)
 
     def run(self):
-        print("[rulesUpdater] Started at: ", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        logger.info(f"[rulesUpdater] Started at: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}")
 
         newData = airtabler.getAllData("rules")
-        print(f"[rulesUpdater] Obtained rules data")
+        logger.info(f"[rulesUpdater] Obtained rules data")
 
         with open("data/rules.json", "w") as f:
             
             f.write(json.dumps(newData))
-        print(f"[rulesUpdater] Updated rules.json")
+        logger.info(f"[rulesUpdater] Updated rules.json")
 
 
 
 
 
-        print("[rulesUpdater] Finished at: ", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        logger.info(f"[rulesUpdater] Finished at: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}")
 
     def schedule(self):
        
         schedule.every().hour.at(":00").do(self.run)
-
-        for minute in range(30):
-            schedule.every().day.at(f"08:{minute:02d}:00").do(self.run)
