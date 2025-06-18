bc23327d26e2c35ddae5ea9aa71513f7cf89de3d
Afonso Inglês <afonsorodriguesingles@gmail.com>
2025-06-19 00:40:59 +0100

    Removing the old sign-up route

--- api/routers/accounts.py
+++ api/routers/accounts.py
@@ -9,31 +9,6 @@ authTools = AuthenticationTools()
 
 
 
-@router.post("v1/accounts/signup")
-async def signup(request: Request):
-    body = await request.json()
-    email = body.get("email")
-    username = body.get("username")
-    password = body.get("password")
-
-    if not email or not username or not password:
-        raise exceptions.BadRequest("Email, username and password are required", "missing_fields")
-
-    try:
-        existingAccount = await authTools.get_user(email)
-        if existingAccount["username"] == username:
-            raise exceptions.BadRequest("A account with this username already exists", "username_in_use")
-        
-        return exceptions.BadRequest("A account with this email already exists", "email_in_use")
-    except exceptions.NotFound:
-        pass
-
-
-    await authTools.create_user(username, email, password)
-    
-    token = authTools.create_token(email)
-    return {"token": token}
-
 
 
 @router.post("v1/accounts/login")
