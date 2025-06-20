8346a8e91f3cee443d3b36e452ba535603e27128
Afonso Inglês <afonsorodriguesingles@gmail.com>
2025-06-20 23:24:41 +0100

    Adding sentry to API

--- api/main.py
+++ api/main.py
@@ -3,6 +3,7 @@ from fastapi.responses import JSONResponse
 import logging
 from utils.github import GitHub
 import api.errors.exceptions as exceptions
+from main import logger
 from api.errors.handlers import create_exception_handler
 import api.routers.notifications
 import api.routers.accounts
@@ -23,13 +24,14 @@ github = GitHub()
 
 @app.exception_handler(500)
 async def internalServerError(request, exc):
+    logger.error("[API] Internal Server Error", exc)
     return JSONResponse(
         status_code=500,
         content={"success": False, "message": "Internal Server Error", "type": "unknown"},
     )
 
 @app.exception_handler(405)
-async def methodNotAllowed(request, exc):
+async def methodNotAllowed(request, exc: Exception):
     return JSONResponse(
         status_code=405,
         content={"success": False, "message": "Method Not Allowed", "type": "method_not_allowed"},
@@ -67,3 +69,7 @@ async def root():
 async def health():
     
     return {"success": True, "message": "OK", "commit": github.get_latest_commit()}
+
+@app.get("/debug")
+async def debug():
+    return 1/0
