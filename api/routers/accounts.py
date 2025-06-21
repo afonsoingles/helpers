2dc6a75dc3d296a1a8abc8e2313a631d8f5802a3
Afonso Inglês <afonsorodriguesingles@gmail.com>
2025-06-21 14:57:46 +0100

    Removing the old /v1/accounts/login endpoint, and creating a new one as /v2/accounts/login

--- api/routers/accounts.py
+++ api/routers/accounts.py
@@ -14,28 +14,6 @@ authTools = AuthenticationTools()
 
 
 
-@router.post("/v1/accounts/login")
-async def login(request: Request):
-    body = await request.json()
-    email = body.get("email")
-    password = body.get("password")
-
-    if not email or not password:
-        raise exceptions.BadRequest("Email and password are required", "missing_fields")
-
-    user = await authTools.get_raw_user(email)
-    if not authTools.check_password(password, user["password"]):
-        raise exceptions.BadRequest("Invalid password", "invalid_password")
-
-    if user.get("blocked"):
-        raise exceptions.Forbidden("User is blocked", "user_blocked")
-
-    await authTools.invalidate_cache(email)
-    token = authTools.create_token(email)
-    return {"token": token}
-
-
-
 @router.get("/v1/accounts/me")
 @authRequired
 async def me(request: Request):
@@ -77,3 +55,28 @@ async def v2_signup(request: Request):
     await authTools.create_user(userData)
     
     return {"success": True, "message": "User created successfully. You can now log in."}
+
+
+@router.post("/v2/accounts/login")
+async def v2_login(request: Request):
+    try:
+        body = await request.json()
+    except:
+        raise exceptions.BadRequest("Invalid JSON data provided", "invalid_json")
+
+    email = body.get("email")
+    password = body.get("password")
+
+    if not email or not password:
+        raise exceptions.BadRequest("Email and password are required", "missing_fields")
+
+    user = await authTools.get_user_by_email(email, bypassCache=True, raw=True)
+
+    if not user or not authTools.check_password(password, user["passwordHash"]):
+        raise exceptions.BadRequest("Invalid email or password", "invalid_credentials")
+
+    if user.get("status") == "blocked":
+        raise exceptions.Forbidden("User is blocked.", "user_blocked")
+
+    token = authTools.create_token(email)
+    return {"success": True, "token": token}
