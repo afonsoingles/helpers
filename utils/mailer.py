d17dedf06eb16943ce270496639df414741bd870
Afonso Inglês <afonsorodriguesingles@gmail.com>
2025-08-11 23:13:01 +0100

    add support for email templates in Mailer

--- utils/mailer.py
+++ utils/mailer.py
@@ -1,15 +1,25 @@
 import requests
 import os
-
 class Mailer:
+    def __init__(self, templatesDir="mailers"):
+        self.templatesDir = templatesDir
 
+    def _load_template(self, templateName, **templateVars):
+        path = os.path.join(self.templatesDir, f"{templateName}.txt")
+        with open(path, "r", encoding="utf-8") as f:
+            content = f.read()
+        return content.format(**templateVars)
 
-    def send_email(self, subject=None, text=None, to=None, name=None):
+    def send_email(self, sender, subject, templateName, to=None, **fields):
+        textBody = self._load_emplate(templateName, **fields)
+
         return requests.post(
-            f"https://api.eu.mailgun.net/v3/{os.environ.get("MAILGUN_DOMAIN")}/messages",
+            f"https://api.eu.mailgun.net/v3/{os.environ.get('MAILGUN_DOMAIN')}/messages",
             auth=("api", os.environ.get("MAILGUN_API_KEY")),
-            data={"from": f"{name} <{os.environ.get("MAILGUN_SENDER_EMAIL")}>",
-                "to": f"{to}",
+            data={
+                "from": f"{sender} <{os.environ.get('MAILGUN_SENDER_EMAIL')}>",
+                "to": to,
                 "subject": subject,
-                "text": text
-            }).json()
+                "text": textBody
+            }
+        ).json()
